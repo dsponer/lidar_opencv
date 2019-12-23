01e9bf368bb3c3f4be97e53a6f8a9a14683a9b42
Dsponer <dsponer157@gmail.com>
2019-12-23 18:08:20 +1000

    add data to file

--- lidar_robot/main.py
+++ lidar_robot/main.py
@@ -1,8 +1,9 @@
 import rplidar
 import numpy as np
 import cv2
+import math
 
-lidar = rplidar.RPLidar('COM6')
+lidar = rplidar.RPLidar('/dev/cu.SLAB_USBtoUART')
 
 info = lidar.get_info()
 print(info)
@@ -11,22 +12,40 @@ lidar.clear_input()
 health = lidar.get_health()
 print(health)
 
-distance = np.zeros(360)
+angles = np.zeros(361)
+distances = np.zeros(361)
+
+start_point = (200, 200)
+
+image = np.zeros((400, 400, 3), np.uint8)
+
+# while True:
 
 for i, angle in enumerate(lidar.iter_measurments()):
-    distance[int(angle[2])] = angle[3]
-    if i > 3000:
+    angles[int(angle[2])] = angle[2]
+    distances[int(angle[2])] = angle[3]
+    if i > 2000:
         break
 
-np.savetxt('lidar.txt', distance)
+for i in range(distances.size):
+    dist = distances[i] / 1000 if distances[i] != 0 else 1
 
-cv2.imshow('test window', distance)
-cv2.waitKey()
+    end_x = math.asin(math.radians(angles[0])) / (dist)
+    end_y = math.acos(math.radians(angles[0])) / (dist)
+
+    end_point = [int(end_x), int(end_y)]
+    print(end_point)
+    image = cv2.line(image, start_point, tuple(end_point), (0, 255, 0), 5)
+    cv2.imshow('test window', image)
+
+# if cv2.waitKey(1) & 0xFF == ord('q'):
+#     break
+
 cv2.destroyAllWindows()
-
-
-print(' '.join(str(i) for i in distance))
 
 lidar.stop()
 lidar.stop_motor()
 lidar.disconnect()
+
+cv2.imshow('test window', image)
+cv2.waitKey()
